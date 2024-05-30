7688d45e7397383ea7647c911a4977762caa2036
Fenosoa RAJAONARIVONY <fenosoa.rajaonarivony@uha.fr>
2024-05-30 12:01:41 +0200

    ajout du fichier mouvement et supression de salma

--- src/mouvement.py
+++ src/mouvement.py
@@ -1,6 +1,3 @@
-# TODO : - Vitesse linéaire et angulaires sur des graphiques. On pourra utiliser le package « matplotlib »
-# - Position relative du robot sur un plan 2D. On pourra également utiliser « matplotlib »
-# - Les valeurs doivent s’actualiser au moins 2 fois par secondes
 import json
 from pathlib import Path
 import numpy as np
